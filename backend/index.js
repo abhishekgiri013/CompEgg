6135208ff07e6d95d52b47aaf6b44ae3825005cd
ABHISHEK GIRI <98091185+abhishekgiri013@users.noreply.github.com>
2024-09-28 14:18:34 +0530

    pre deployement changes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js'
-import cookieparser from 'cookie-parser'
+import cookieparser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO)
@@ -12,8 +13,17 @@ mongoose.connect(process.env.MONGO)
         })
         .catch((err)=>{
             console.log(err)
-        })
+        });
+
+const __dirname = path.resolve();
+
 const app = express();
+
+ app.use(express.static(path.join(__dirname,'client/dist')))
+ app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist' ,'index.html'))
+ })
+
 app.use(express.json());
 
 app.use(cookieparser());
